f338f57ce8fd9e954070e863150e9ef890e50ee0
Ricky Dunlop <ricky@rehabstudio.com>
2015-01-16 18:06:31 +0000

    Added tests

---
+++ app/src/androidTest/java/com/rickydunlop/sunshine/FullTestSuite.java
@@ -0,0 +1,20 @@
+package com.rickydunlop.sunshine;
+
+/**
+ * Created by ricky on 16/01/2015.
+ */
+import android.test.suitebuilder.TestSuiteBuilder;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class FullTestSuite extends TestSuite {
+    public static Test suite() {
+        return new TestSuiteBuilder(FullTestSuite.class)
+                .includeAllPackagesUnderHere().build();
+    }
+
+    public FullTestSuite() {
+        super();
+    }
+}
